051628b88d1610d9f85f1ea659299288d95a9cd9
JonahMMay <119529402+JonahMMay@users.noreply.github.com>
2022-11-30 14:44:48 -0600

    Delete SSL_Replace.ps1

--- VBR/Cloud Connect/SSL_Replace.ps1
+++
@@ -1,9 +0,0 @@
-# Replace the cloud gateway certificate with a new one that is located in the certificate store
-
-Connect-VBRServer -Server localhost
-
-$certificate = Get-VBRCloudGatewayCertificate -FromStore | Where {$_.SubjectName -eq "CN=vspc.example.com"} | Sort-Object -Property NotBefore -Descending | Select -First 1
-
-Add-VBRCloudGatewayCertificate -Certificate $certificate
-
-Disconnect-VBRServer
